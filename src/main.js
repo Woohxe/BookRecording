fbe998fea739fe04ee42ce61c74b955b82ce3677
Wuxiaoer <773577909@qq.com>
2023-02-11 00:12:10 +0800

    配置修改

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// 给 axios 设置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:3000/api/'
+app.config.globalProperties.$axios = axios
+
+app.mount('#app')
