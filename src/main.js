d78bbfaff311712a5084ac70cc996a7a57c431f6
Wuxiaoer <773577909@qq.com>
2023-02-12 13:02:43 +0800

    引入router、store等

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import store from './store'
+import router from './router'
+
+import ElementPlus, { ElMessage } from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import Axios from './http'
 
 const app = createApp(App)
 // 给 axios 设置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:3000/api/'
-app.config.globalProperties.$axios = axios
+app.config.globalProperties.$axios = Axios
+app.config.globalProperties.$message = ElMessage;
 
-app.mount('#app')
+app.use(router).use(store).use(ElementPlus).mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
