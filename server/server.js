fda2c912cd5e6db264f3bbe7f75cf6d037a53128
Wuxiaoer <773577909@qq.com>
2023-02-12 13:01:43 +0800

    新增书籍相关路由

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({ extended: false }))
 const adminRouter = require('./router/admin')
 app.use('/api/admin', adminRouter)
 
+//书籍查询增加相关路由
+const bookRouter = require('./router/book')
+app.use('/api/book', bookRouter)
 
 // 调用app.listen方法，指定端口号并启动web服务器
 app.listen(3000, () => {
