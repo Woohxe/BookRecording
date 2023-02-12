03345dd2e266ec4765be6b8ee4210f2c994cf5d6
Wuxiaoer <773577909@qq.com>
2023-02-12 13:06:22 +0800

    新增首页并绑定相应组件

---
+++ src/views/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+        <HeaderNav />
+        <LeftNav />
+        <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import HeaderNav from "@/components/HeaderNav.vue"
+import LeftNav from "@/components/LeftNav.vue"
+</script>
+
+<style scoped>
+.main {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
