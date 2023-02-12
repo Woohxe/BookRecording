35a40efaa85c8aa72483c04383e2f672f5535242
Wuxiaoer <773577909@qq.com>
2023-02-12 13:07:03 +0800

    初始化，新增登录状态的判断

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
-  <register/>
+  <router-view />
 </template>
 
-<script>
-import register from './views/register.vue'
+<script setup>
+import jwt_decode from "jwt-decode";
+import { useStore } from "vuex";
+import { onMounted } from "vue";
+const store = useStore();
 
-export default {
-  name: 'App',
-  components: {
-    register
+// 判断token是否为空
+const isEmpty = (value) => {
+  return (
+    value === undefined ||
+    value === null ||
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  );
+};
+
+onMounted(() => {
+  if (localStorage.bookRecordToken) {
+    const decoded = jwt_decode(localStorage.bookRecordToken);
+    // token存储到vuex中
+    store.dispatch("setAuthenticated", !isEmpty(decoded));
+    store.dispatch("setUser", decoded);
   }
-}
+});
 </script>
 
 <style>
-
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  height: 100vh;
+}
 </style>
